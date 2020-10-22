f914b82a92805a6cc310294d962c7cc6bb2eb32e
Hans Accola <Hansolo6283@gmail.com>
2020-10-21 22:15:30 -0500

    updated database.sql

--- database.sql
+++ database.sql
@@ -8,3 +8,24 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "adventure" (
+	"id" SERIAL PRIMARY KEY,
+	"date" DATE DEFAULT CURRENT_DATE,
+	"park_name" TEXT,
+	"image_url" TEXT NOT NULL,
+	"street" TEXT,
+	"city" TEXT,
+	"state" TEXT NOT NULL,
+	"zip_code" INT,
+	"main_activities" TEXT NOT NULL,
+	"description" TEXT NOT NULL,
+	"completed" BOOLEAN DEFAULT FALSE,
+	"user_id" INT REFERENCES "user"
+);
+
+CREATE TABLE "user_adventure_likes" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" INT REFERENCES "user",
+	"adventure_id" INT REFERENCES "adventure"
+);
